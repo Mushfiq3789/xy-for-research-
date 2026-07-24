9959945b0da5f0160ae40885856ee0d0ea54720e
Md Mushfiqur Rahman <rahmanmushfiq301@gmail.com>
2026-07-24 18:46:08 +0600

    intro

---
+++ data/raw/ps.R
@@ -0,0 +1,74 @@
+#Gettting Help
+?mean 
+# 1. Variables
+# Assignment
+help("reserved")
+a = 10
+
+b <-10
+
+# Naming convention
+age<-20 
+my_age <- 10
+students_height<- 5.5
+
+# Reserved Keywords
+?Reserved 
+
+# Data types 
+# Number 
+num1<- 30
+num2<- 45.5
+# check type of variable 
+class(num1)
+class(num2)
+
+# text data / character 
+name <- "Mushfiq"
+class(name)
+
+# Logical Data (TRUE/T . FATSE/F)
+is_weekday<-T
+class(is_weekday)
+
+# Arithmetic Operators 
+num3<- 10 
+num4<- 5
+
+
+num3+num4
+num3-num4
+num3*num4
+num3/num4
+
+num3%% 2
+num3^ 2
+
+
+# relational operators(TRUE/FALSE)
+2==2
+2>1
+2<1
+4 <=5 
+5>=4
+2!=4
+
+# logical operation
+# logical AND 
+2==2 & 2> 1
+
+2==2 & 2< 1
+
+# logical OR
+2==2 | 2> 1
+
+2==2 | 2< 1
+
+
+#logical NOT 
+
+!2==2
+
+
+
+
